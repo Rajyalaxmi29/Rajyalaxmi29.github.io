00e16aaed557a38399bb4863f14be4653aafdc0d
Rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-05-13 19:32:55 +0530

    update

--- a6/script.js
+++ a6/script.js
@@ -1,12 +1,13 @@
 
-    function greet() {
+   const greet =()=>{
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const phone=document.getElementById("phone").value;
 
   const output = `Email: ${email}<br>Password: ${password}<br>Phone no.: ${phone}`;
   document.getElementById("output").innerHTML = output;
-}
+};
+
 
 
 
---
+++ a7/script.js
@@ -0,0 +1,28 @@
+const greet = () => {
+  // Convert input values to numbers using Number() or parseFloat()
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+
+  function add(a, b) {
+    return a + b;
+  }
+  let result1 = add(num1, num2);
+
+  function sub(a, b) {
+    return a - b;
+  }
+  let result2 = sub(num1, num2);
+
+  function mul(a, b) {
+    return a * b;
+  }
+  let result3 = mul(num1, num2);
+
+  function div(a, b) {
+    return a / b;
+  }
+  let result4 = div(num1, num2);
+
+  const output = `Addition: ${result1}<br>Subtraction: ${result2}<br>Multiplication: ${result3}<br>Division: ${result4}`;
+  document.getElementById("output").innerHTML = output;
+};
