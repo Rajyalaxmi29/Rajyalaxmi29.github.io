f8f8c39ad7a885344a75854d3982c412948cc0f4
Rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-05-15 16:31:20 +0530

    up

--- a8/script.js
+++ a8/script.js
@@ -1,6 +1,5 @@
 
 
-
    function calculate(op) {
             let num1 = parseFloat(document.getElementById("num1").value);
             let num2 = parseFloat(document.getElementById("num2").value);
@@ -8,20 +7,18 @@
 
            
                 switch(op) {
-                    case '+': result = `Addition: ${num1 + num2}`; 
+                    case '+': result = ("addition"+(num1+num2));
                     break;
                     case '-': result = `Subtraction: ${num1 - num2}`; 
                     break;
                     case '*': result = `Multiplication: ${num1 * num2}`; 
                     break;
-                    case '/': 
-                        result = num2 !== 0 ? `Division: ${num1 / num2}` : "Cannot divide by zero";
+                    case '/': result = num2 !== 0 ? `Division: ${num1 / num2}` : "Cannot divide by zero";
                         break;
                 }
             
             document.getElementById("result").innerText = result;
         }
-
 
 
 
