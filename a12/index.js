d913fe73e097dd0e5aa43471497501eae0626dec
Rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-05-16 19:29:54 +0530

    up

---
+++ a12/index.js
@@ -0,0 +1,8 @@
+const product = [
+    {id:1,name:"apple",price:23},
+     {id:2,name:"orange",price:13},
+      {id:3,name:"banana",price:29}
+
+];
+
+product.forEach((product)=>console.log(product.name))
