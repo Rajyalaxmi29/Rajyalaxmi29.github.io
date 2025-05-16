ec876e9f885f70c895848e71599d22ad386dfc28
Rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-05-16 19:36:02 +0530

    update

---
+++ a13/index.js
@@ -0,0 +1,10 @@
+
+const product = [
+    {id:1,name:"apple",price:23},
+     {id:2,name:"orange",price:13},
+      {id:3,name:"banana",price:29}
+
+];
+
+product.forEach((product)=>console.log(product.price+5))
+product.forEach((product)=>console.log(product.name))
