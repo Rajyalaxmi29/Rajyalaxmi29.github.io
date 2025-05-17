3163ad47a691f1cbea7e72d6a45ad423005737dd
Rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-05-17 21:00:57 +0530

    up

--- a16/index.js
+++ a16/index.js
@@ -1,4 +1,47 @@
-function greet(){
+//change add function to return promise
+
+/*function add(x,y){
+    return Promise.resolve(x+y);
+}
+function sqrt(v){
+    return v*v;
+}
+async function main(){
+let sum= await add(4,5)
+let result=sqrt(sum)
+console.log(result)
+}
+main()*/
+
+//if x is less then 5 then reject and print x is less than 5
+// the promise in add else reolve
+
+function add(x,y){
+    return new Promise((resolve,reject)=>{
+        if(x<5){
+            reject("x is less than 5")
+        }
+        else{
+            resolve(x+y)
+        }
+    })
+}
+function sqrt(v){
+    return v*v;
+}
+async function main(){
+    try{
+let sum= await add(10,5)
+let result=sqrt(sum)
+console.log(result)
+} catch (error){
+    console.log(error)
+}
+}
+main()
+
+
+/*function greet(){
   console.log("Hello World")
 }
  
@@ -9,8 +52,4 @@ async function dispData() {
   greet()
 }
 dispData();
-
- 
-
-
- 
+*/
