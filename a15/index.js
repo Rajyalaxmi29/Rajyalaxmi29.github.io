8d0514bdbd1b477300236f8635d544bddc56b183
Rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-05-17 19:12:05 +0530

    up

--- a15/index.js
+++ a15/index.js
@@ -1,4 +1,43 @@
+// adding total column
+
 const product = [
+    {id:1,name:"apple",price:23,qty:3,status:"pending"},
+     {id:2,name:"orange",price:13,qty:4,status:"pending"},
+      {id:3,name:"banana",price:29,qty:2,status:"pending"}
+
+];
+
+//print name,price,qty,total where qty>2
+
+let updatedProducts = product.map((product) => ({
+  ...product,price:product.price+5,status:"complete"
+  ,name:product.name,total:product.price*product.qty
+}));
+
+let pro = updatedProducts.filter((value) => {
+  return value.qty > 2; 
+});
+
+console.log(pro);
+
+
+
+
+
+
+/*let updatedProducts = product.map((product) => ({
+  ...product,price:product.price+5,status:"complete"
+  ,name:product.name,total:product.price*product.qty
+}));
+
+//console.log(updatedProducts)
+updatedProducts.forEach(element => {
+  console.log(element.name+" "+element.total);
+})*/
+
+
+
+/*const product = [
     {id:1,name:"apple",price:23,status:"pending"},
      {id:2,name:"orange",price:13,status:"pending"},
       {id:3,name:"banana",price:29,status:"pending"}
@@ -8,4 +47,13 @@ const product = [
 let updatedProducts = product.map((product) => ({
   ...product,price:product.price+5,status:"complete"
 }));
-console.log(updatedProducts)
+//console.log(updatedProducts)
+
+let first = updatedProducts.map((value) => {
+    return {
+        name: value.name,
+        status: value.status,
+    };
+});
+console.log(first);*/
+
